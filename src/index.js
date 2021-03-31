52d36cf115168fdafaf81740daf205a5ee800cea
spiguer <nunoecb@gmail.com>
2021-03-31 02:01:24 +0100

    Adicionado código ao index para ir buscar código das páginas

--- src/index.js
+++ src/index.js
@@ -1,10 +1,23 @@
 const express = require('express')
+const path = require('path')
+const hbs = require('hbs')
 require('./db/mongoose')
 const adminRouter = require('./routers/admin')
 const cursoRouter = require('./routers/cursos')
 
 const app = express()
-const port = process.env.PORT
+const port = process.env.PORT || 3000
+
+const publicDirectoryPath = path.join(path.join(__dirname, '../public'))
+const viewsPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
+
+app.set('view engine', 'hbs')
+app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
+
+app.use(express.static(publicDirectoryPath))
+
 
 app.use(express.json())
 app.use(adminRouter)
